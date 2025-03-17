abcc7d68468c7cbb046f13d87797680d063b31ef
DavidBi19 <davidbinder194@gmail.com>
2025-03-17 15:55:09 +0100

    Mergefix

--- Program.py
+++ Program.py
@@ -11,37 +11,30 @@
 
 ### Variablen ###
 
-Buffer_Zeile = [float(0), float(0), float(0), float(0), float(0)]   #Bufferzeile später zum Tauschen von Zeilen verwendet 
+Buffer_Zeile = [float(0), float(0), float(0), float(0), float(0)]
 Status = "OK"
-X1 = 0
-X2 = 0
-X3 = 0
-X4 = 0
-Null_Status = 0                                                     #Wird 1 wenn an der 1.Stelle einer Zeile eine Null steht
-Fehler_Counter = 0
+Array_Rang = 0
 Error = False
+Buffer_Loesung = float(0)
+LGS_korrektur = "Ja"
 
 ### Programm ###
 
 while True:
     
-    # Eingabe:
+    ### Eingabe:
     
     Error = False
     Status = "OK"
 
+    # Anzahl der Variablen festlegen:
+
     try:
-        print("Gebe dein Lineares Gleichungssystem im folgenden Format ein:")
-        print("A11 A12 A13 A14 | A1A")
-        print("A21 A22 A23 A24 | A2A")
-        print("A31 A32 A33 A34 | A3A")
-        print("A41 A42 A43 A44 | A4A")
-        
-        Zeile1 = [float(input("A11 = ")), float(input("A12 = ")), float(input("A13 = ")), float(input("A14 = ")), float(input("A1A = "))]
-        Zeile2 = [float(input("A21 = ")), float(input("A22 = ")), float(input("A23 = ")), float(input("A24 = ")), float(input("A2A = "))]
-        Zeile3 = [float(input("A31 = ")), float(input("A32 = ")), float(input("A33 = ")), float(input("A34 = ")), float(input("A3A = "))]
-        Zeile4 = [float(input("A41 = ")), float(input("A42 = ")), float(input("A43 = ")), float(input("A44 = ")), float(input("A4A = "))]
+
+        Array_Rang = int(input("Wie viele Variablen hat dein LGS: "))
+
     except ValueError:
+
         Error = True
         Status = "Eingabefehler"
 
@@ -194,59 +187,49 @@ while True:
 
     if (Error == False):
 
-        if (Zeile3[2] != 0): 
-    
-            if (Zeile4[2] != 0):
-            
-                Buffer1 = Zeile3[2]
-                Buffer2 = Zeile4[2]
-                Buffer_Zeile = Zeile3
-                Buffer_Zeile = [i * Buffer2 for i in Buffer_Zeile]
-                Zeile4 = [i * Buffer1 for i in Zeile4]
-
-                if ((Buffer_Zeile[2] < 0) and (Zeile4[2] < 0)) or ((Buffer_Zeile[2] > 0) and (Zeile4[2] > 0)):
-                    for i in range(5):
-                        Zeile4[i] -= Buffer_Zeile[i]
-
-                elif ((Buffer_Zeile[2] < 0) and (Zeile4[2] > 0)) or ((Buffer_Zeile[2] > 0) and (Zeile4[2] < 0)):
-                    for i in range(5):
-                        Zeile4[i] += Buffer_Zeile[i]
-
-        else: 
-            Buffer_Zeile = Zeile3
-            Zeile3 = Zeile4
-            Zeile4 = Buffer_Zeile
-    
-    if (Status == "OK"):
         try:
-            X4 = (Zeile4[4] / Zeile4[3])
-            X3 = ((Zeile3[4] - (Zeile3[3] * X4)) / Zeile3[2])
-            X2 = ((Zeile2[4] - (Zeile2[3] * X4) - (Zeile2[2] * X3)) / Zeile2[1])
-            X1 = ((Zeile1[4] - (Zeile1[3] * X4) - (Zeile1[2] * X3) - (Zeile1[1]) * X2) / Zeile1[0])
+
+            for i in range((Array_Rang - 1), -1, -1):
+
+                Buffer_Loesung = LGS[i][Array_Rang]
+                
+                for a in range(Array_Rang):
+
+                    Buffer_Loesung -= ((LGS[i][a]) * (Loesung[a]))
+
+                Loesung[i] = Buffer_Loesung / LGS[i][i]
+
         except ZeroDivisionError:
+
+            Error = True
             Status = "Nullzeile"
 
-    # Ausgabe:
+    ### Ausgabe:
 
-    if Error == False:
+    # Ausgabe bei korrekter Berechnung:
+
+    if (Error == False):
     
-        print("Dreiecksmatrix:")
-        print(Zeile1)
-        print(Zeile2)
-        print(Zeile3)
-        print(Zeile4)
+        print("Dreiecksmatrix: \n")
 
-        print("Status:", Status)
+        for Zeilen in LGS:
+
+            print(Zeilen)
+
+        print("\nStatus:", Status, "\n")
 
         if Status == "OK":
 
-            print("Dein Ergebnis lautet:")
-            print("X1 = ", X1)
-            print("X2 = ", X2)
-            print("X3 = ", X3)
-            print("X4 = ", X4)
+            print("Dein Ergebnis lautet: \n")
+
+            for i in range(Array_Rang):
+
+                print(f"{Variablen[i]} = {Loesung[i]}\n")
+
+    # Ausgabe bei Fehler:
 
     else:
         
-        print("Es gab einen Fehler, bitte gebe dein LGS erneut ein")
-    
+        print(f"Es gab folgenden Fehler: {Status} \nBitte gebe dein LGS erneut ein \n")
+
+### Programm Ende ###   
